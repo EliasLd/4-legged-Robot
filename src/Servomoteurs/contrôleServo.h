64c1d09aaa8acec7968c7afc01f505b657f80437
Elias <elabd.elias@gmail.com>
2024-02-26 16:22:25 +0100

    dep & servos init

---
+++ src/Servomoteurs/contrôleServo.h
@@ -0,0 +1,6 @@
+#include <Servo.h>
+
+void Forward(int speed, Servo servoR, Servo servoL);
+void Backward(int speed, Servo servoR, Servo servoL);
+void Right(int speed, Servo servoR, Servo servoL);
+void left(int speed, Servo servoR, Servo servoL)
