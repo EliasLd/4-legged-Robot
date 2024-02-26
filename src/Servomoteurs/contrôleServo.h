9ed9219e07f691cb4d9c12aa758facba48b73106
Elias <elabd.elias@gmail.com>
2024-02-26 16:27:19 +0100

    servo funcs def

--- src/Servomoteurs/contrôleServo.h
+++ src/Servomoteurs/contrôleServo.h
@@ -3,4 +3,5 @@
 void Forward(int speed, Servo servoR, Servo servoL);
 void Backward(int speed, Servo servoR, Servo servoL);
 void Right(int speed, Servo servoR, Servo servoL);
-void left(int speed, Servo servoR, Servo servoL)
+void left(int speed, Servo servoR, Servo servoL);
+
---
+++ src/Servomoteurs/contrôleServos.cpp
@@ -0,0 +1,26 @@
+#include <Servo.h>
+#include <Arduino.h>
+
+void Forward(int speed, Servo servoR, Servo servoL){
+  servoR.writeMicroseconds(1500 + speed);
+  servoL.writeMicroseconds(1500 + speed);
+  delay(10);
+}
+
+void Backward(int speed, Servo servoR, Servo servoL){
+  servoR.writeMicroseconds(1500 - speed);
+  servoL.writeMicroseconds(1500 - speed);
+  delay(10);
+}
+
+void Right(int speed, Servo servoR, Servo servoL){
+  servoR.writeMicroseconds(1500 - speed);
+  servoL.writeMicroseconds(1500 + speed);
+  delay(10);
+}
+
+void left(int speed, Servo servoR, Servo servoL){
+  servoR.writeMicroseconds(1500 + speed);
+  servoL.writeMicroseconds(1500 - speed);
+  delay(10);
+}
